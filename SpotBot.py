62fdf3e2e262e4843f89bffe8d3659d31009d8bf
Miles Oldenburger <32121785+60MilesPerHour@users.noreply.github.com>
2023-01-22 00:40:03 -0800

    SpotBot.py

---
+++ SpotBot.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3.8
+import re
+import os
+
+def SpotBot():
+    spotify_list = open('/home/miles/Music/SpotList.txt', 'r')
+    bitrate = input('Enter the bitrate you want to download the music at (128k, 256k, 320k): ')
+    os.chdir('/home/miles/Music')
+
+    music = []
+    for line in spotify_list:
+        if line.startswith('Album: ') or line.startswith('album: '):
+            music.append(line[7:-1].rstrip())
+            continue
+        elif line.startswith('Playlist: ') or line.startswith('playlist: '):
+            music.append(line[10:-1].rstrip())
+            continue
+        elif line.startswith('Song: ') or line.startswith('song: '):
+            music.append(line[6:-1].rstrip())
+            continue
+        else:
+            continue
+
+    spotify_links = []
+
+    with open("/home/miles/Music/SpotList.txt", 'r') as file:
+
+            for line in file:
+                albums = re.findall('https://open.spotify.com/album/.*', line)
+                playlists = re.findall('https://open.spotify.com/playlist/.*', line)
+                songs = re.findall('https://open.spotify.com/track/.*', line)
+                for url in albums:
+                    spotify_links.append(url)
+                    continue
+                for url in playlists:
+                    spotify_links.append(url)
+                    continue
+                for url in songs:
+                    spotify_links.append(url)
+                    continue
+                continue
+
+    for i in range(len(music)):
+        os.mkdir(music[i])
+
+    for i in range(len(music)):
+        os.chdir(music[i])
+        print('Downloading ' + music[i] + '...' + '\n')
+        os.system('spotdl --bitrate ' + bitrate + spotify_links[i])
+        os.chdir('..')
+        os.system('clear')
+    spotify_list.close()
+SpotBot()
